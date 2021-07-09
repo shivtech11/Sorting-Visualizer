49d116168778d7c7002d871d8afedb7b55027396
Shivam Sharma <shiv16085@gmail.com>
2021-07-09 13:32:52 +0530

    adding port

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get("/",function(req,res){
 });
 
 
-app.listen(3000,function(){
-  console.log("port 3000");
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started Successfully");
+});
